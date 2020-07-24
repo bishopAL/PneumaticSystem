fc8676910bf111d44084f2870f23fc877e1630dc
Wang Bingcheng <470797339@qq.com>
2020-07-24 15:54:41 +0800

    basicly finish

--- pneumatic_system/PneumaticSys.cpp
+++ pneumatic_system/PneumaticSys.cpp
@@ -3,28 +3,32 @@
 
 PneumaticSys::PneumaticSys()
 {
-    for(int i=0; i<4; i++)  //i=0:4 <-> 脚顺序：RF RH LF LH
+    for(int i=0; i<4; i++)  //i=0:4 <-> 脚顺序：LF RF RH LH
     {
-        for(int j=0; j<4; j++)
+        for(int j=0; j<3; j++)
         {
-            SV[i][j] = i*4 + j + 22;
+            SV[i][j] = i*3 + j + 2;
         }
     }
-
+    SV[3][0] = SV[3][0] + 3; //最后三个电磁阀用14，15，16，把digital口给电气比例阀用
+    SV[3][1] = SV[3][1] + 3;
+    SV[3][2] = SV[3][2] + 3;
   	for(int i=0; i<4; i++)
   	{
-  		pinMode(SV[i][1], OUTPUT); //一号电磁阀，正压气路 所有电磁阀设为断电常闭
-   		pinMode(SV[i][2], OUTPUT); //二号电磁阀，负压气路
-      pinMode(SV[i][3], OUTPUT); //三号电磁阀，泄压阀
-      pinMode(SV[i][4], OUTPUT); //四号电磁阀，总阀门
+  		pinMode(SV[i][0], OUTPUT); //一号电磁阀，正压气路 所有电磁阀设为断电常闭
+   		pinMode(SV[i][1], OUTPUT); //二号电磁阀，负压气路
+      pinMode(SV[i][2], OUTPUT); //三号电磁阀，泄压阀
+      //pinMode(SV[i][3], OUTPUT); //四号电磁阀，总阀门
   	}
     for(int i=0; i<4; i++)
     {
-        digitalWrite(SV[i][1], LOW); //一号电磁阀，正压气路 所有电磁阀设为断电常闭
-        digitalWrite(SV[i][2], LOW); //二号电磁阀，负压气路
-        digitalWrite(SV[i][3], LOW); //三号电磁阀，泄压阀
-        digitalWrite(SV[i][4], LOW); //四号电磁阀，总阀门
+        digitalWrite(SV[i][0], LOW); //一号电磁阀，正压气路 所有电磁阀设为断电常闭
+        digitalWrite(SV[i][1], LOW); //二号电磁阀，负压气路
+        digitalWrite(SV[i][2], LOW); //三号电磁阀，泄压阀
+        //digitalWrite(SV[i][3], LOW); //四号电磁阀，总阀门
     }
+    pinMode(12, OUTPUT); // 电气比例阀1号，负责充气气压
+    pinMode(13, OUTPUT); // 电气比例阀2号，负责吸气气压
 }
 
 
@@ -33,34 +37,34 @@ PneumaticSys::~PneumaticSys()
   Serial.println("Done!");
 }
 
-void PneumaticSys::enableNormalPneumatic(int i)
+void PneumaticSys::enableNormalPneumatic(int i) // 
 {
-	digitalWrite(SV[i][1],LOW);
-    digitalWrite(SV[i][2],LOW);
-    digitalWrite(SV[i][3],HIGH);
-    digitalWrite(SV[i][4],HIGH);
+	  digitalWrite(SV[i][0],LOW);
+    digitalWrite(SV[i][1],LOW);
+    digitalWrite(SV[i][2],HIGH);
+    //digitalWrite(SV[i][3],HIGH);
 }
 
 void PneumaticSys::enablePositivePneumatic(int i)
 {
-    digitalWrite(SV[i][1],HIGH);
+    digitalWrite(SV[i][0],HIGH);
+    digitalWrite(SV[i][1],LOW);
     digitalWrite(SV[i][2],LOW);
-    digitalWrite(SV[i][3],LOW);
-    digitalWrite(SV[i][4],HIGH);
+    //digitalWrite(SV[i][3],HIGH);
 }
 
 void PneumaticSys::enableNegtivePneumatic(int i)
 {
-    digitalWrite(SV[i][1],LOW);
-    digitalWrite(SV[i][2],HIGH);
-    digitalWrite(SV[i][3],LOW);
-    digitalWrite(SV[i][4],HIGH);
+    digitalWrite(SV[i][0],LOW);
+    digitalWrite(SV[i][1],HIGH);
+    digitalWrite(SV[i][2],LOW);
+    //digitalWrite(SV[i][3],HIGH);
 }
 
 void PneumaticSys::holdPressure(int i)
 {
+    digitalWrite(SV[i][0],LOW);
     digitalWrite(SV[i][1],LOW);
     digitalWrite(SV[i][2],LOW);
-    digitalWrite(SV[i][3],LOW);
-    digitalWrite(SV[i][4],LOW);
+    //digitalWrite(SV[i][3],LOW);
 }
--- pneumatic_system/pneumatic_system.ino
+++ pneumatic_system/pneumatic_system.ino
@@ -1,4 +1,8 @@
 #include "PneumaticSys.hpp"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 PneumaticSys ps;
 char temp[4];
@@ -6,16 +10,40 @@ uint8_t *presentPressureStatus = new uint8_t[4]; //当前的气压状态
 uint8_t *previousPressureStatus = new uint8_t[4]; //先前的气压状态
 uint8_t *operatingArray =  new uint8_t[4];
 
+uint8_t *separateFlag(int flag)
+{
+  uint8_t *res = new uint8_t[4];
+  res[0] = flag / 1000;
+  res[1] = (flag - res[0]*1000) / 100;
+  res[2] = (flag - res[0]*1000 - res[1]*100) / 10;
+  res[3] = (flag - res[0]*1000 - res[1]*100 - res[2]* 10) / 1;
+  return res;
+}
+
 void setup() {
-  Serial1.begin(9600);
   Serial.begin(9600);
-  Serial1.setTimeout(50);
   Serial.setTimeout(50);
   Serial.println("Serial Initialize Done!");
   for (int i=0; i<4; i++)
   {
     previousPressureStatus[i] = 0;
+    presentPressureStatus[i] = 0;
   }
+  Serial.print("当前电磁阀编号状态：");
+  for(int i=0; i<4; i++)  //i=0:4 <-> 脚顺序：RF RH LF LH
+    {
+        for(int j=0; j<3; j++)
+        {
+            Serial.print(ps.SV[i][j]);
+            Serial.print(",");
+        }
+    }
+   Serial.println(" ");
+
+   analogWrite(12, int(0.03/0.9*255)); // 充气电气比例阀电压设置
+   analogWrite(13, int(0.1/0.9*255)); // 吸气电气比例阀电压设置
+   lcd.init(); 
+   lcd.backlight();
 }
 
 
@@ -23,59 +51,58 @@ void loop()
 {
   String temp_string;
   int temp_int;
-  if (Serial1.available()>0) 
+  if (Serial.available()>0) 
   {   
-    Serial1.readBytes(temp, 4);
-    if(temp[0] > 48 && temp[1] > 48 && temp[2] > 48 &&temp[3] > 48 )
+//    temp_string = Serial.readString();
+//    temp_int = temp_string.toInt();
+//    presentPressureStatus = separateFlag(temp_int);
+    lcd.setCursor(0, 0);
+    lcd.print("S");
+    Serial.readBytes(temp, 4);
+    
+    Serial.print("temp：");
+    if(temp[0] >= 48 && temp[1] >= 48 && temp[2] >= 48 &&temp[3] >= 48 )
     {
       for(int i=0; i<4; i++)
       {
         presentPressureStatus[i] = temp[i] - 48;
       }
     }
+    
+    lcd.setCursor(8, 0);
+    lcd.print(presentPressureStatus[0]);
+    lcd.setCursor(10, 0);
+    lcd.print(presentPressureStatus[1]);
+    lcd.setCursor(8, 1);
+    lcd.print(presentPressureStatus[3]);
+    lcd.setCursor(10, 1);
+    lcd.print(presentPressureStatus[2]);
   }
-        
-   for (int i=0; i<4; i++)
+    for (int i=0; i<4; i++)
    {
-      if (previousPressureStatus[i]!=presentPressureStatus[i]) operatingArray[i] = 1; //判断有哪几个与之前的气压状态不同，不同为1，相同为0
-      else operatingArray[i] = 0;
+        switch(presentPressureStatus[i])
+        {
+          case 0:ps.enableNormalPneumatic(i);  break;
+          case 1:ps.enablePositivePneumatic(i);  break;
+          case 2:ps.enableNegtivePneumatic(i);  break;
+        }
    }
-   
-   if(operatingArray[0] == 1 or operatingArray[1] == 1 or operatingArray[2] == 1 or operatingArray[3] == 1)
-   {
-      for (int i=0; i<4; i++)
-     {
-        if (operatingArray[i] == 1) 
-        {
-          Serial.print("改变数：");
-          Serial.println(i);
-        }
-        {
-          switch(presentPressureStatus[i])
-          {
-            case 0:ps.enableNormalPneumatic(i); break;
-            case 1:ps.enablePositivePneumatic(i); break;
-            case 2:ps.enableNegtivePneumatic(i); break;
-          }
-        }
-     }
-     delay(500);
-      for (int i=0; i<4; i++)
-     {
-        if (operatingArray[i] == 1) 
-        {
-           ps.holdPressure(i);
-        }
-     }
-     previousPressureStatus = presentPressureStatus;
+//     delay(500);
+//      for (int i=0; i<4; i++)
+//     {
+//        if (operatingArray[i] == 1) 
+//        {
+//           ps.holdPressure(i);
+//        }
+//     }
      Serial.print("当前状态：");
      for(int i=0; i<4; i++)
      {   
-        Serial.print(previousPressureStatus[i]);
+        Serial.print(presentPressureStatus[i]);
         Serial.print(",");
      }
      Serial.println(" ");
-   }    
+     
       
   
 }
